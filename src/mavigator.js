d03cdb7d44a97e0b5b5c60b1a68d9d0a4113e50d
Kfir Ben-Ami <kfirba2@gmail.com>
2018-01-19 17:04:34 +0200

    add support for 'depth' matching and cover uri edge cases

--- src/mavigator.js
+++ src/mavigator.js
@@ -1,5 +1,6 @@
 class Mavigator {
     constructor(selector = 'html', options) {
+        // TODO: If the given selector is an object, we assume the user simply passed the options instead of throwing an error.
         this.selector = selector;
         this.options = this.mergeOptions(Mavigator.defaults(), options);
     }
@@ -13,6 +14,7 @@ class Mavigator {
             className: 'active',
             classToParent: false,
             uri: window.location.pathname,
+            markTreeDepth: 0,
             warn: false
         };
     }
@@ -30,7 +32,7 @@ class Mavigator {
     init() {
         let nodes = this.getNodesToMark();
 
-        if ( ! nodes || ! nodes.length) {
+        if (! nodes || ! nodes.length) {
             if (this.options.warn) {
                 console.warn(`No link to mark was found for the given URI [${this.options.uri}]`);
             }
@@ -48,7 +50,7 @@ class Mavigator {
         let nodes = [];
         let sets = this.sets();
 
-        if ( ! sets.length) return;
+        if (! sets.length) return;
 
         for (let i = 0; i < sets.length; i++) {
             nodes = nodes.concat(this.getMarkableNodesFrom(sets[i]));
@@ -64,7 +66,7 @@ class Mavigator {
     }
 
     validateSelector() {
-        if ( ! this.selector.length) {
+        if (! this.selector.length) {
             throw new TypeError('The provided selector is empty.');
         }
 
@@ -74,11 +76,52 @@ class Mavigator {
     }
 
     getMarkableNodesFrom(set) {
-        let selector = 'a';
-        let links = set.querySelectorAll(selector);
-        links = [].slice.call(links);
+        const possibleMatches = this.generatePossibleMatches();
+        const links = this.getLinksFrom(set);
+        
+        return links.filter(link => possibleMatches.includes(link.pathname));
+    }
 
-        return links.filter(link => link.pathname === this.options.uri);
+    generatePossibleMatches() {
+        let uri = this.options.uri;
+        uri = uri.charAt(0) === '/' ? uri : `/${uri}`;
+        const possibilities = [uri];
+
+        // If the URI ends with a '/', remove it and add it as a possibility.
+        if (uri.charAt(uri.length - 1) === '/') {
+            possibilities.push(uri.slice(0, uri.length - 1));
+        }
+
+        return ! this.options.markTreeDepth ? possibilities : this.extendPossibilites(possibilities);
+    }
+
+    extendPossibilites(possibilities) {
+        const uri = this.options.uri.charAt(0) === '/' ? this.options.uri.slice(1) : this.options.uri;
+        const segments = uri.split('/');
+        this.options.markTreeDepth = this.clamp(this.options.markTreeDepth, -1, segments.length - 1);
+        let depth = [];
+
+
+        if (this.options.markTreeDepth === -1) {
+            for (let i = 0; i < segments.length; i++) {
+                let lastDepth = i - 1 in depth ? depth[i - 1] : '';
+                depth.push(`${lastDepth}/${segments[i]}`);
+            }
+        }
+        
+        for (let i = 1; i <= this.options.markTreeDepth; i++) {
+            depth.push(`/${segments.slice(0, segments.length - i).join('/')}`);
+        }
+
+        return possibilities.concat(depth);
+    }
+
+    clamp(number, min, max) {
+        return Math.min(Math.max(number, min), max);
+    }
+
+    getLinksFrom(set) {
+        return [].slice.call(set.querySelectorAll('a'));
     }
 
     addClassTo(node) {
